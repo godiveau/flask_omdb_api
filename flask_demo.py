837a49824ca20b4d202dbce04fbe90c66fbdb2ec
Benoit <benoit@hopsandfork.com>
2015-01-23 15:44:47 +0100

    Add tests for the format function
    Your mission is to fixed the red test that happen when you search a
    movie with an accent.

--- flask_demo.py
+++ flask_demo.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, url_for, render_template, request
+from format_name import *
 import requests
 
 app = Flask(__name__)
@@ -6,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def api_root():
     if request.method == 'POST':
-        movie_name=request.form['movie_name'].strip().replace(" ","+")
+        movie_name= format_name(request.form['movie_name'])
         url = "http://www.omdbapi.com/?t={0}&y=&plot=short&r=json".format(movie_name)
         omdb_request = requests.get(url)
         omdb_result = omdb_request.json()
---
+++ format_name.py
@@ -0,0 +1,2 @@
+def format_name(movie_name):
+    return movie_name.strip().replace(" ","+")
